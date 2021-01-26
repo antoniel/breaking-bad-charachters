cbac197a71f31d035f8d40addbcbb617b90d52ea
Antoniel Magalhães <antoniel2210@gmail.com>
2021-01-26 12:46:41 -0300

    feat: add appears in seasons field

--- src/components/CharachterInfo/index.tsx
+++ src/components/CharachterInfo/index.tsx
@@ -7,7 +7,7 @@ export const CharacterInfo = () => {
           src="https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg"
           alt="CharacterPicture"
         />
-        <h2 className="text-2xl font-extrabold text-center py-2 text-gray-300">
+        <h2 className="py-2 text-2xl font-extrabold text-center text-gray-300">
           Walter White
         </h2>
       </article>
@@ -15,6 +15,11 @@ export const CharacterInfo = () => {
         <DataFieldText title="Nickname" content="Walter white" />
         <DataFieldText title="Ocupation" content={["string", "string"]} />
         <DataFieldText title="Status" content="Walter white" />
+        <DataFieldText
+          title="Appear in seasons: "
+          content={["1", "2", "3", "4", "5"]}
+          type="season"
+        />
       </article>
     </section>
   );
@@ -23,21 +28,40 @@ export const CharacterInfo = () => {
 interface DataFieldTextProps {
   title: string;
   content: string | string[];
+  type?: "season";
 }
 
 export const DataFieldText: React.FC<DataFieldTextProps> = ({
   title,
   content,
+  type,
 }) => {
-  if (typeof content === "object") {
+  if (typeof content === "object" && type !== "season") {
     return (
       <section className="text-gray-300">
         <h4 className="font-extrabold ">{title}</h4>
         <div className="space-y-2">
-          {content.map((value) => (
+          {content.map((value, idx) => (
             <div
-              key={value}
+              key={idx}
               className="flex items-center h-8 px-4 align-middle rounded-lg card-gradient"
+            >
+              <p>{value}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
+  if (typeof content === "object" && type === "season") {
+    return (
+      <section className="text-gray-300">
+        <h4 className="font-extrabold ">{title}</h4>
+        <div className="flex space-x-4">
+          {content.map((value, idx) => (
+            <div
+              key={idx}
+              className="flex items-center w-16 h-16 align-middle rounded-lg card-gradient justify-center"
             >
               <p>{value}</p>
             </div>
